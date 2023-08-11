9b99b0e008e0475af1892dd6cfa8d65442b6af5b
Sinh <107097399+sinhkhmer@users.noreply.github.com>
2023-08-11 18:57:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 
+const videoElement = document.getElementById('camera');
+
+async function startBackCamera() {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: {
+                facingMode: 'environment' // Use the back camera
+            }
+        });
+        videoElement.srcObject = stream;
+    } catch (error) {
+        console.error('Error accessing back camera:', error);
+    }
+}
+
+startBackCamera();
+
+// Load the pre-trained model for object recognition
+(async function() {
+    const model = await tf.loadLayersModel('path/to/model.json');
+
+    // Capture a frame from the video
+    const frame = await captureFrame();
+
+    // Preprocess the frame and make predictions
+    const preprocessedFrame = preprocessFrame(frame);
+    const predictions = await model.predict(preprocessedFrame);
+
+    // Process predictions and display results
+    displayPredictions(predictions);
+})();
+
+// Capture a frame from the video
+async function captureFrame() {
+    const canvas = document.createElement('canvas');
+    canvas.width = videoElement.videoWidth;
+    canvas.height = videoElement.videoHeight;
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+    return tf.browser.fromPixels(canvas);
+}
+
+// Preprocess the frame
+function preprocessFrame(frame) {
+    // Perform preprocessing steps based on the model's requirements
+    // Example: resizing the frame, normalizing pixel values, etc.
+    return tf.tidy(() => {
+        // Preprocess the frame here
+        return preprocessedFrame;
+    });
+}
+
+// Display object recognition predictions
+function displayPredictions(predictions) {
+    // Process predictions and update the UI
+    // Example: displaying the recognized objects and their probabilities
+}
